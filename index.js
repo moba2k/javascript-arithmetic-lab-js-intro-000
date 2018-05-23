ca204e50a79951f6970add7d53c484602b337fdf
Mobasher Sobhan <moba2k@yahoo.com>
2018-05-23 17:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-1 + 80
+var a = 10, b = 5;
+
+function add(a , b) {
+  return (a + b);
+}
+
+function subtract(a , b) {
+  return (a - b);
+}
+
+function multiply(a , b) {
+  return (a * b);
+}
+
+function divide (a , b) {
+  return (a / b);
+}
+
+var n = 40;
+
+function inc(n) {
+  return n = n + 1;
+}
+
+var n = 13;
+
+function dec(n) {
+  return n = n - 1;
+}
